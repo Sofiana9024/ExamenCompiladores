f3a8ed24e24b0cc8d085e99a37a524cf3372817d
Scanfmonse <monseestrada2001@gmail.com>
2025-04-04 20:10:00 -0600

    Codigo del profe

--- archivo.cpp
+++ archivo.cpp
@@ -1,12 +1,5 @@
 #include <iostream>
-using namespace std;
-
-int suma(int a, int b) {
-    return a + b;
-}
 
 int main() {
-    int resultado = suma(5, 10);
-    cout << "El resultado es: " << resultado << endl; // como se cuentan los << del cout ????
-    return 0;
+    cout<<"Hola"; // como se cuentan los << del cout ????
 }
